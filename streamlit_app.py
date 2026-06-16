62c9ff99da5f98e1779062ed145a8215596a5f4b
KC117 <kachunlai117@gmail.com>
2026-06-16 15:32:17 +0100

    Fix string concatenation in API request URL

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ if ingredients_list:
     
     for fruits_chosen in ingredients_list:
         ingredients_string += fruits_chosen + ' '
-        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{fruits_chosen}")
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruits_chosen)
         sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
       
     #st.write(ingredients_string)
