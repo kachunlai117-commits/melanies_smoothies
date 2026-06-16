1efb68a5b9622dfd5952813f42faf073e43f8a8a
KC117 <kachunlai117@gmail.com>
2026-06-16 16:03:06 +0100

    Fix variable name for fruit search in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,11 +27,11 @@ if ingredients_list:
     
     for fruits_chosen in ingredients_list:
         ingredients_string += fruits_chosen + ' '
-        search_on = pd_df['SEARCH_ON'][pd_df['FRUIT_NAME'] == fruit_chosen].iloc[0]
-        st.write('The Search Value for ', fruit_chosen, ' is ', search_on, '.')
+        search_on = pd_df['SEARCH_ON'][pd_df['FRUIT_NAME'] == fruits_chosen].iloc[0]
+        st.write('The Search Value for ', fruits_chosen, ' is ', search_on, '.')
         sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
-        st.subheader(fruit_chosen + ' Nutrition Information ')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit" + search_on)
+        st.subheader(fruits_chosen + ' Nutrition Information ')
+        fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{search_on}")
       
     #st.write(ingredients_string)
 
